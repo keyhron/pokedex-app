43dde16da47023314556790fe5a24f517ee6a1f1
Carlos Barrios <juanbarrios045@gmail.com>
2023-05-19 21:47:09 -0400

    Fix svg and edit scripts in package json

--- src/components/Atoms/Loader.tsx
+++ src/components/Atoms/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <svg className="loader" viewBox="25 25 50 50" stroke-width="5">
+    <svg className="loader" viewBox="25 25 50 50" strokeWidth="5">
       <circle cx="50" cy="50" r="20" />
     </svg>
   );
